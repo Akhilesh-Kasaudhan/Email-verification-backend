76c97943cac726d049f7c2b9bf1edea33443e803
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-01-02 01:51:11 +0530

    frontend url

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 connectDB();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://email-verification-cunning.netlify.app",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(cookiParser());
 app.use(express.urlencoded({ extended: true }));
